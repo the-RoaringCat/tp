61ab939c7998d59a47c03fa6ad7f27799ecfa5a0
Mo Hanqi <mohanqi666@gmail.com>
2026-04-02 01:56:07 +0800

    Enhance `Command`: Add methods for help and flag validation; update visibility of `ui` and `parser`.

--- src/main/java/seedu/goldencompass/command/Command.java
+++ src/main/java/seedu/goldencompass/command/Command.java
@@ -5,10 +5,10 @@ import seedu.goldencompass.parser.Parser;
 import seedu.goldencompass.ui.Ui;
 
 public class Command implements Executable {
-
+    private static final String FLAG_MISSING_MESSAGE = "Error: This command requires this flag: ";
     /** Package-private UI and parser */
-    Ui ui;
-    Parser parser;
+    protected Ui ui;
+    protected Parser parser;
 
     /** No-argument constructor, automatically called in the constructors of subclasses */
     public Command() {
@@ -23,6 +23,32 @@ public class Command implements Executable {
     @Override
     public void execute() throws GoldenCompassException {
 
+    }
+
+    private void printHelp(String commandDescription, String flagDescription) {
+        ui.print(commandDescription, flagDescription);
+    }
+
+    protected boolean checkHelpFlag(String commandDescription, String flagDescription) {
+        if(parser.isFlagExist("/help")) {
+            printHelp(commandDescription, flagDescription);
+            return true;
+        }
+        return false;
+    }
+
+    protected void checkFlagPresence(String... flags) throws GoldenCompassException {
+        StringBuilder errorMessage = new StringBuilder();
+
+        for(String flag : flags) {
+            if(!parser.isFlagExist(flag)) {
+                errorMessage.append(FLAG_MISSING_MESSAGE).append(flag).append(System.lineSeparator());
+            }
+        }
+
+        if(!errorMessage.isEmpty()) {
+            throw new GoldenCompassException(errorMessage.toString());
+        }
     }
 
 }
